4bbaf72d7af4e4733d29d542be3a662605d5bfcc
unknown <jaydaworld@icloud.com>
2023-07-25 11:54:16 -0500

    first commit

---
+++ src/api/server.ts
@@ -0,0 +1,81 @@
+const token = '6dbd802752f8ab030fa9446584899aec9d8ac89569981e0f'
+
+export const server_calls = {
+    get: async () => { 
+        const response = await fetch(`https://flask-car-collection.onrender.com/api/contacts`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            }
+
+        });
+
+        if (!response.ok){
+            throw new Error('Failed to fetch data from the server')
+        }
+
+        return await response.json()
+    },
+
+    create: async (data: any = {}) => {
+        const response = await fetch(`https://flask-car-collection.onrender.com/api/contacts`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to create new data on the server')
+        }
+
+        return await response.json()
+    },
+
+    update: async (id: string, data:any = {}) => {
+        const response = await fetch(`https://flask-car-collection.onrender.com/api/contacts/${id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to update data on the server')
+        }
+
+        return await response.json()
+    },
+
+    delete: async (id: string) => {
+        const response = await fetch(`https://flask-car-collection.onrender.com/api/contacts/${id}`,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'x-access-token': `Bearer ${token}`
+            },
+
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to delete data from the server')
+        }
+
+        return;
+    },
+}
---
+++ src/config/auth0.config.ts
@@ -0,0 +1,4 @@
+export const auth0Config = {
+    domain: "dev-nw6eeuapshvklk4x.us.auth0.com",
+    clientId: "toi72MaYmjxn4j1IbyBGFADt0KEAbP2Q",
+  };
